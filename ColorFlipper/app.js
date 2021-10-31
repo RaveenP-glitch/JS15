f8ba0a71264dc577f9b01a5c9098433c99818ffa
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2021-10-31 23:16:26 +0530

    saveday1

--- ColorFlipper/app.js
+++ ColorFlipper/app.js
@@ -1,14 +1,22 @@
-const colors = ["green", "red", "rgbs(133,122,200)",
+const colors = ["green", "red", "rgba(133,122,200)",
 "#f15025"];
 
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
+
+
+function getRandomArbitrary() {
+    return Math.floor(Math.random() * colors.length);
+}
+
 btn.addEventListener('click', function(){
     //get random number between 0-3
 
-    const randomNumber = 2;
+   
+    var randomNumber = getRandomArbitrary(0,4);
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
     console.log("Color changed");
-})
+
+});
