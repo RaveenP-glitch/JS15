b231823f294d0a344649e769c75ee417eccaea66
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2021-12-31 20:43:06 +0530

    react testing

---
+++ reaction-test/src/components/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 
+
+function Homepage() {
+    return (
+        <div>
+            <h1>Hello Everyone, We are learning reactJS.</h1>
+        </div>
+    )
+}
+
+export default Homepage;
